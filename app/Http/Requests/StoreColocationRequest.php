d98c15bbfe86bed8c8bcb2cf345ab75f61ff2d99
Ilyass Mouqerassou <ilyassmouqerassou0@gmail.com>
2026-02-26 10:22:05 +0000

    fix : update colocation request validation

--- app/Http/Requests/StoreColocationRequest.php
+++ app/Http/Requests/StoreColocationRequest.php
@@ -11,7 +11,7 @@ class StoreColocationRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +22,7 @@ class StoreColocationRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'name' => 'required|string|min:3|max:100|unique:colocations,name',
+            'name' => 'required|string|min:3|max:100',
             'description' => 'required|string',
             'status' => 'required|in:active,cancelled',
         ];
