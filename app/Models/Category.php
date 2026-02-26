617758b9c16be366484877b449d09bfefb6ddeac
Ilyass Mouqerassou <ilyassmouqerassou0@gmail.com>
2026-02-26 01:27:01 +0000

    feat : Add relations to Category model

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -3,11 +3,23 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Category extends Model
 {
     protected $table = 'category';
 
-    protected $fillable = ['colocation_id','name'];
-    
+    protected $fillable = ['colocation_id', 'name'];
+
+    public function colocation(): BelongsTo
+    {
+        return $this->belongsTo(Colocation::class);
+    }
+
+    public function expenses(): HasMany
+    {
+        return $this->hasMany(Expense::class);
+    }
+
 }
