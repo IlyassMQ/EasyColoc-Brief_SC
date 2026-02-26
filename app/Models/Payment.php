3e0069db89d5adbffa0a6362ae06513ea36ab303
Ilyass Mouqerassou <ilyassmouqerassou0@gmail.com>
2026-02-26 01:31:50 +0000

    feat : Add relations to Payment model

--- app/Models/Payment.php
+++ app/Models/Payment.php
@@ -7,4 +7,19 @@ use Illuminate\Database\Eloquent\Model;
 class Payment extends Model
 {
     protected $fillable =['payer_id','receiver_id','colocation_id','amount','paid_at'];
+
+    public function payer()
+    {
+        return $this->belongsTo(User::class, 'payer_id');
+    }
+
+    public function receiver()
+    {
+        return $this->belongsTo(User::class, 'receiver_id');
+    }
+
+    public function colocation()
+    {
+        return $this->belongsTo(Colocation::class);
+    }
 }
