f8157b4c9472e35550d7968d736446dcfdd5bcd9
Ilyass Mouqerassou <ilyassmouqerassou0@gmail.com>
2026-02-26 01:33:56 +0000

    feat : Add relations to Invitation model

--- app/Models/Invitation.php
+++ app/Models/Invitation.php
@@ -3,8 +3,19 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Invitation extends Model
 {
-    protected $fillable = ['colocation_id','user_id','email','token','status'];
+    protected $fillable = ['colocation_id', 'user_id', 'email', 'token', 'status'];
+
+    public function colocation(): BelongsTo
+    {
+        return $this->belongsTo(Colocation::class);
+    }
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
