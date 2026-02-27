09d7769a08a87fe4ef05b6c83270107ffd842ae2
Ilyass Mouqerassou <ilyassmouqerassou0@gmail.com>
2026-02-27 00:28:39 +0000

    feat : add user ban and unban actions

--- routes/web.php
+++ routes/web.php
@@ -16,9 +16,21 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-    //Admins routes
+    
     
 });
+//Admins routes
 
-Route::get('/admin/dashboard', [AdminController::class,'index'])->name('dashboard');
+
+
+Route::prefix('admin')->name('admin.')->group(function () {
+
+    Route::get('/dashboard',[AdminController::class,'index']);
+    Route::post('/users/{user}/ban', [AdminController::class, 'ban'])
+        ->name('users.ban');
+
+    Route::post('/users/{user}/unban', [AdminController::class, 'unban'])
+        ->name('users.unban');
+});
+
 require __DIR__.'/auth.php';
