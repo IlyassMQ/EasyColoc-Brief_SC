6ebeb27ebebb8d6167eef50c0053ea7d25b6ae5a
Ilyass Mouqerassou <ilyassmouqerassou0@gmail.com>
2026-03-03 23:05:26 +0000

    feat : add check if banned middleware

---
+++ app/Http/Middleware/CheckIfBanned.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Illuminate\Support\Facades\Auth;
+
+class CheckIfBanned
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        if (auth()->check() && auth()->user()->banned_at !== null) {
+
+           Auth::logout();
+
+            return redirect()->route('login')
+                ->with('error', 'Votre compte a été suspendu.');
+        }
+
+        return $next($request);
+    }
+}
